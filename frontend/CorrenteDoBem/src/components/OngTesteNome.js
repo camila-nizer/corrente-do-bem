0789b6e3820e9008dd2967985318c643656f9101
Camila Nizer <contato.camilanizer@gmail.com>
2024-12-01 17:09:46 -0300

    update

--- frontend/CorrenteDoBem/src/components/OngTesteNome.js
+++ frontend/CorrenteDoBem/src/components/OngTesteNome.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 import { Dropdown } from 'react-native-element-dropdown';
 import AntDesign from '@expo/vector-icons/AntDesign';
